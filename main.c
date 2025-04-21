8fe420551c940186f405829536cb62cb04b283d0
Drekkou <67373616+Drekkou@users.noreply.github.com>
2025-04-22 00:56:22 +0200

    Bon je crois bien que j'ai rien compris a ce que je devais appeler pour vérifier si une grille est bonne, mais tout le reste fonctionne

--- main.c
+++ main.c
@@ -27,21 +27,72 @@ Grille jouer_coup(Grille G){
     return G;
 }
 
-void start(Grille grille){
-    int joue = 1;
+
+int fin_partie(Grille G,char *nom_fichier){
+    FILE *f;
+    char fich_dimacs[50];
+    int i,res;
+    char command[150] = "minisat ";
+    char *fin_executable = " output.txt";
+
+    // Création du fichier dimacs avec le nom du fichier grille mais en .cnf
+    strcpy(fich_dimacs,nom_fichier);
+    i = 0;
+    while(fich_dimacs[i] !='.' && fich_dimacs[i] !='\0'){
+    
+      i++;
+    
+    }
+    fich_dimacs[i+1]='c';
+    fich_dimacs[i+2]='n';
+    fich_dimacs[i+3]='f';
+    fich_dimacs[i+4]='\0';
+
+    f = fopen(fich_dimacs,"w"); // On ouvre le fichier .cnf
+    ecriture_dimacs(f,G); // le fclose(f); est dans la fonction
+     
+    strcat(command,fich_dimacs);
+    strcat(command,fin_executable);
+    
+    system(command);
+
+    f = fopen("output.txt","r");
+    res = lecture_fich(f,G);
+    afficher_Grille(G);
+
+    if(res == 0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+void start(Grille grille, char *nom_fichier){
+    int joue = 1, res;
     char retour_joueur;
     printf("Vous jouez avec la grille suivante : \n");
 
     afficher_Grille(grille);
     while(joue){
-        printf("boucle\n");
         jouer_coup(grille);
 
         printf("Voulez-vous essayer ce résultats ? (Y/N) : ");
         scanf("%s",&retour_joueur);
         if(strcmp(&retour_joueur, "N")){
-            // fin_partie(grille);
-            joue = 0;
+            res = fin_partie(grille,nom_fichier);
+
+            if(res == 0){
+                printf("Votre solution est correcte !\n");
+                joue = 0;
+            }else{
+                printf("Votre solution est incorrecte.\n");
+                printf("Voulez-la modifier? (Y/N):");
+                scanf("%s",&retour_joueur);
+                if(strcmp(&retour_joueur, "Y")){
+                    joue = 0;
+                }else{
+                    joue = 1;
+                }
+            }
         }else{
             joue = 1;
         }
@@ -68,12 +119,13 @@ int main(int argc, char* argv[]){
     }
     printf("\nBienvenue dans le jeu Light Up\n\n");
     Grille grille = lire_Grille(f);
+    fclose(f);
 
     retour_joueur = 'Y';
 
     while(jouer){
         if(strcmp(&retour_joueur, "N")){
-            start(grille);
+            start(grille, argv[1]);
             printf("Voulez-vous rejouer ? (Y/N) : ");
             scanf("%s",&retour_joueur);
         }else{
